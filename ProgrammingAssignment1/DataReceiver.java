d7c3be5614a4de3945c83995da8bce1344c996f8
MajdeJudeh <mbjudeh3@uno.edu>
2018-02-18 16:23:13 -0600

    Updated pa1

--- ProgrammingAssignment1/DataReceiver.java
+++ ProgrammingAssignment1/DataReceiver.java
@@ -8,15 +8,36 @@ public class DataReceiver {
     ObjectInputStream in = new ObjectInputStream(dataReceiver.getInputStream());
     ){
       int[] intArray;
+      String data;
+      long time = System.currentTimeMillis(), previous = 0;
+      long previousTime = 0;
+      long averageTime = 0;
+      double rate = 0.0;
       while((intArray = (int[])in.readObject()) != null){
-        System.out.println(intArray);
+
+
+        previous = System.currentTimeMillis();
+        data = "Data: ";
+        for(int i =0; i < intArray.length; i++){
+          data += intArray[i] + " ";
+        }
+
+        System.out.println(data);
+
+        time = previous - time;
+        previousTime = time;
+        averageTime = (previousTime + time)/2;
+        rate = 1000/averageTime;
+        System.out.println(averageTime);
+        System.out.println(rate);
+
+        time = System.currentTimeMillis();
       }
 
-
     } catch (IOException e){
-
+      System.out.println("IOException");
     } catch (ClassNotFoundException e){
-
+      System.out.println("ClassNotFoundException");
     }
   }
 }
